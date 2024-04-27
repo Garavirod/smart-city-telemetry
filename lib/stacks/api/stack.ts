5d30cbe6c522992ecf5b877fa2c293a0308cef7b
Garavirod <rodrigogarciaavila26@gmail.com>
2024-04-27 14:19:58 -0600

    feat(iam): add im role for github actions

--- lib/stacks/api/stack.ts
+++ lib/stacks/api/stack.ts
@@ -1,15 +1,12 @@
+import { Construct } from "constructs";
 import { GeneralApiGateway } from "./general-apigw";
 import { createAllAPIResources } from "./resources";
+import * as cdk from 'aws-cdk-lib';
 
-
-export class APIGatewayStack {
-  private scope: any;
-  constructor(scope: any) {
-    this.scope = scope;
-  }
-
-  public createStack() {
-    GeneralApiGateway.Instance.initConfiguration(this.scope);
+export class APIGatewayStack extends cdk.Stack{
+  constructor(scope: Construct, id: string, props?: cdk.StackProps){
+    super(scope, id, props);
+    GeneralApiGateway.Instance.initConfiguration(this);
     createAllAPIResources();
   }
 }
---
+++ lib/stacks/trust-gh-actions/stack.ts
@@ -0,0 +1,94 @@
+import * as cdk from "aws-cdk-lib";
+import * as iam from "aws-cdk-lib/aws-iam";
+import { Construct } from "constructs";
+export class TrustGHActionsStack extends cdk.Stack {
+  private githubOrg: cdk.CfnParameter;
+  private githubRepo: cdk.CfnParameter;
+  private githubProvider: iam.CfnOIDCProvider;
+  private githubActionsRole: iam.Role;
+  private assumeCdkDeploymentRoles: iam.PolicyStatement;
+
+  constructor(scope: Construct, id: string, props: cdk.StackProps) {
+    super(scope, id, props);
+    this.configureProvider();
+    this.createIamRole();
+    this.createPolicY();
+  }
+
+  private configureProvider() {
+    this.githubOrg = new cdk.CfnParameter(this, "GithubOrg", {
+      type: "String",
+      description: "The GitHub organization that owns the repository.",
+    });
+
+    this.githubRepo = new cdk.CfnParameter(this, "GitHubRepo", {
+      type: "String",
+      description: "The GitHub repository that will run the action.",
+    });
+
+    this.githubProvider = new iam.CfnOIDCProvider(this, "GitHubOIDCProvider", {
+      thumbprintList: ["6938fd4d98bab03faadb97b34396831e3780aea1"],
+      url: "https://token.actions.githubusercontent.com", // <-- 1 per account
+      clientIdList: ["sts.amazonaws.com"], // <-- Tokens are intended for STS
+    });
+  }
+
+  private createIamRole() {
+    this.githubActionsRole = new iam.Role(this, "GitHubActionsRole", {
+      assumedBy: new iam.FederatedPrincipal(
+        this.githubProvider.attrArn,
+        {
+          StringLike: {
+            // This specifies that the subscriber (sub) claim must be the main
+            // branch of your repository. You can use wildcards here, but
+            // you should be careful about what you allow.
+            "token.actions.githubusercontent.com:sub": [
+              `repo:${this.githubOrg.valueAsString}/${this.githubRepo.valueAsString}:ref:refs/heads/main`,
+            ],
+          },
+          // This specifies that the audience (aud) claim must be sts.amazonaws.com
+          StringEquals: {
+            "token.actions.githubusercontent.com:aud": "sts.amazonaws.com",
+          },
+        },
+        "sts:AssumeRoleWithWebIdentity" // <-- Allows use of OIDC identity
+      ),
+    });
+  }
+
+  private createPolicY() {
+    // -- A policy to permit assumption of the default AWS CDK roles. --
+    // Allows assuming roles tagged with an aws-cdk:bootstrap-role tag of
+    // certain values (file-publishing, lookup, deploy) which permit the CDK
+    // application to look up existing values, publish assets, and create
+    // CloudFormation changesets. These roles are created by CDK's
+    // bootstrapping process. See:
+    // https://docs.aws.amazon.com/cdk/latest/guide/bootstrapping.html
+    //
+    // WARNING: The CDK `deploy` role allows the CDK to execute changes via
+    //          CloudFormation with its execution role. The execution role
+    //          has full administrative permissions. It can only be assumed
+    //          by CloudFormation, but you should still be aware.
+    this.assumeCdkDeploymentRoles = new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: ["sts:AssumeRole"],
+      resources: ["arn:aws:iam::*:role/cdk-*"],
+      conditions: {
+        StringEquals: {
+          "aws:ResourceTag/aws-cdk:bootstrap-role": [
+            "file-publishing",
+            "lookup",
+            "deploy",
+          ],
+        },
+      },
+    });
+
+    // Attach policy to role
+    this.githubActionsRole.addToPolicy(this.assumeCdkDeploymentRoles);
+    new cdk.CfnOutput(this, "GitHubActionsRoleArn", {
+      value: this.githubActionsRole.roleArn,
+      description: "The role ARN for GitHub Actions to use during deployment.",
+    });
+  }
+}
