ddcd5ae5dd2566cd593ed73a298ebee3a41044c7
Garavirod <rodrigogarciaavila26@gmail.com>
2024-06-07 11:30:57 -0600

    feat(TelemetryStack): split the websocket builder up in different files

--- bin/smart-city-telemetry.ts
+++ bin/smart-city-telemetry.ts
@@ -1,12 +1,10 @@
 #!/usr/bin/env node
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
-import { createSvcApisStack } from "../lib/stacks/svc-api-gateway-stack/stack";
-
-
+import { createTelemetryStack } from "../lib/stacks/svc-api-gateway-stack/stack";
 
 const app = new cdk.App();
-createSvcApisStack(app);
+createTelemetryStack(app);
 // createStackSvcWebSocketApi(app)
 // Stacks creation
 // new SvcPermissionsStack(app, "svc-permissions-stack",{});
--- lib/stacks/svc-api-gateway-stack/stack.ts
+++ lib/stacks/svc-api-gateway-stack/stack.ts
@@ -1,6 +1,4 @@
 import { App, Stack } from "aws-cdk-lib";
-import { WebSocketCDKBuilder } from "../../libs/cdk-builders/web-socket-api";
-import { LambdasFunctionNames } from "../shared/enums/lambdas";
 import { createTables } from "./cdk/builders/dynamo";
 import { createCognitoPools } from "./cdk/builders/cognito";
 import { createUsersLambdas } from "./cdk/builders/lambda/users";
@@ -8,72 +6,47 @@ import { createDependenciesLambdas } from "./cdk/builders/lambda/dependencies";
 import { createWebSocketConnLambdas } from "./cdk/builders/lambda/web-socket-connections";
 import { createTrainsLambdas } from "./cdk/builders/lambda/trains";
 import { createRestApi } from "./cdk/builders/api";
+import {
+  createWebSocketApi,
+  createWebSocketRoutes,
+} from "./cdk/builders/websocket";
 
-export function createSvcApisStack(app:App) {
-    const stack = new Stack(app, "SvcApis");
-    // Websocket settings
-    const websocket = WebSocketCDKBuilder.createWebsocket({
-      scope: stack,
-      webSocketNameId: "Web-socket=api",
-      webSocketDescription: `Web socket for the resource web-socket=api`,
-    });
+export function createTelemetryStack(app: App) {
+  const stack = new Stack(app, "TelemetryStack");
+  // Websocket settings
+  const websocket = createWebSocketApi(stack);
+  // Dynamo Settings
+  const dynamoTables = createTables(stack);
 
-    WebSocketCDKBuilder.createStage({
-      scope: stack,
-      stageId: "WebSocketStage",
-      stageName: "dev", // TODO set this according environment,
+  // Cognito settings
+  const { userPoolClients, userPools } = createCognitoPools(stack);
+  const cognitoUserPools = userPools;
+  const cognitoUserPoolClients = userPoolClients;
+
+  // Lambda settings
+  const lambdaFunctions = {
+    ...createUsersLambdas({
       webSocket: websocket,
-    });
-
-    // Dynamo Settings
-    const dynamoTables = createTables(stack);
-
-    // Cognito settings
-    const { userPoolClients, userPools } =
-      createCognitoPools(stack);
-    const cognitoUserPools = userPools;
-    const cognitoUserPoolClients = userPoolClients;
-
-    // Lambda settings
-    const lambdaFunctions = {
-      ...createUsersLambdas({
-        webSocket: websocket,
-        tables: dynamoTables,
-        cognitoClients: cognitoUserPoolClients,
-        cognitoPools: cognitoUserPools,
-        stack,
-      }),
-      ...createDependenciesLambdas(stack, dynamoTables),
-      ...createWebSocketConnLambdas(stack, dynamoTables),
-      ...createTrainsLambdas(stack, dynamoTables),
-    };
-
-    // Rest api
-    const restApi = createRestApi({
-      lambdas: lambdaFunctions,
+      tables: dynamoTables,
+      cognitoClients: cognitoUserPoolClients,
       cognitoPools: cognitoUserPools,
       stack,
-    });
+    }),
+    ...createDependenciesLambdas(stack, dynamoTables),
+    ...createWebSocketConnLambdas(stack, dynamoTables),
+    ...createTrainsLambdas(stack, dynamoTables),
+  };
 
-    // Create Websocket route
-    /* This is t he route where the GPS train sensor will publish the message (Locations) */
-    WebSocketCDKBuilder.createRoute({
-      webSocket: websocket,
-      routeName: "$connect",
-      integration:
-        lambdaFunctions[LambdasFunctionNames.CreateNewConnection],
-    });
-    WebSocketCDKBuilder.createRoute({
-      webSocket: websocket,
-      routeName: "$disconnect",
-      integration: lambdaFunctions[LambdasFunctionNames.DeleteConnection],
-    });
+  // Rest api
+  const restApi = createRestApi({
+    lambdas: lambdaFunctions,
+    cognitoPools: cognitoUserPools,
+    stack,
+  });
 
-    WebSocketCDKBuilder.createRoute({
-      webSocket: websocket,
-      routeName: "trainLocation",
-      integration:
-      lambdaFunctions[LambdasFunctionNames.NotifyTrainLocation],
-    });
-  }
-
+  // Add Websocket route
+  createWebSocketRoutes({
+    lambdas: lambdaFunctions,
+    webSocket: websocket,
+  });
+}
